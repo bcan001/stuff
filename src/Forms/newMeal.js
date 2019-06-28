e08394033373861f7bbfd2ad13a10d1286eacbe6
Ben <ben@ben.kleinschmidt.com>
2019-06-28 16:34:50 -0500

    all

--- src/Forms/newMeal.js
+++ src/Forms/newMeal.js
@@ -1,20 +1,60 @@
 import React, { Component } from "react";
 
-export default class newMeal extends Component {
-	// super(props);
-	state = {
-		// day_id: 1,
-	}
-	// <span style={{ flex: 1 }}><button>add meal(transition to mealForm)</button></span>
+// export default class newMeal extends Component {
+// 	constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
+// 	// day_id: 1,
+// 	// <span style={{ flex: 1 }}><button>add meal(transition to mealForm)</button></span>
+// 	// Title,Day,Date,Time,Total Calories,Health Rating,Foods
 
-	render() {
-    return (
-  		<div>
-        new meal form
-    	</div>
-    )
-  }
-}
+// 	render() {
+// 		// console.log(this.props.newMeal);
+// 		const { newMeal,onChange,saveMeal,removeMeal } = this.props;
+//     return (
+//     	<div>
+// 	  		<form onSubmit={saveMeal.bind(this)}>
+//     			<div style={{ display: 'flex'}}>
+//     				<span style={{ flex: 1 }}>
+// 			      	<input type="text" placeholder="title here" value={newMeal.title} onChange={onChange}/>
+// 			      </span>
+// 			      <span style={{ flex: 1 }}>
+// 			      	&nbsp;
+// 			      </span>
+// 			      <span style={{ flex: 1 }}>
+// 			      	&nbsp;
+// 			      </span>
+// 			      <span style={{ flex: 1 }}>
+// 			      	&nbsp;
+// 			      </span>
+// 			      <span style={{ flex: 1 }}>
+// 			      	&nbsp;
+// 			      </span>
+// 			      <span style={{ flex: 1 }}>
+// 			      	&nbsp;
+// 			      </span>
+// 			      <span style={{ flex: 1 }}>
+// 			      	&nbsp;
+// 			      </span>
+// 			      <span style={{ flex: 1 }}>
+// 			      	<button type="submit">Save Meal</button>
+// 			      </span>
+// 			      <span style={{ flex: 1 }}>
+// 			      	<button onClick={removeMeal.bind(this)}>Remove Meal</button>
+// 			      </span>
+// 			    </div>
+// 		    </form>
+// 		  </div>
+//     )
+//   }
+// }
+
+
+
+
+
+
 
 
 
--- src/Models/meal.js
+++ src/Models/meal.js
@@ -7,8 +7,23 @@ export default class Meal extends Component {
     super(props);
     this.state = {
       foodsAreOpen: false,
-      totalCalories: 0
+      totalCalories: 0,
+      update: props.update,
+      foods: props.meal.foods,
+      id: props.meal.id,
+      title: props.meal.title,
+      day: props.meal.day,
+      date: props.meal.date,
+      time: props.meal.time,
+      total_calories: props.meal.total_calories,
+      health_rating: props.meal.health_rating
     };
+  }
+
+  componentDidMount() {
+    this.setState({
+      totalCalories: calculator.calculateTotalMealCalories(this.props.meal)
+    })
   }
 
   showFoods = () => {
@@ -19,26 +34,80 @@ export default class Meal extends Component {
     }
   }
 
-  componentDidMount() {
+  onUpdateText = event => {
+    console.log('updating text')
+    console.log(event.target)
+    this.setState({ [event.target.id]: event.target.value });
+  };
+
+  saveMeal = e => {
+    e.preventDefault();
+    console.log('meal has been saved')
+
     this.setState({
-      totalCalories: calculator.calculateTotalMealCalories(this.props.meal)
+      update: false
     })
-  }
+
+  };
+
 
   render() {
-    const { meal } = this.props;
+    const { removeMeal } = this.props;
     return (
       <div>
-        <div style={{ display: 'flex'}}>
-          <span style={{ flex: 1 }}>{meal.title}</span>
-          <span style={{ flex: 1 }}>{meal.day}</span>
-          <span style={{ flex: 1 }}>{meal.date}</span>
-          <span style={{ flex: 1 }}>{meal.time}</span>
-          <span style={{ flex: 1 }}>{this.state.totalCalories}</span>
-          <span style={{ flex: 1 }}>{meal.health_rating}</span>
-          <span style={{ flex: 1 }}><button onClick={() => this.showFoods()}>show foods</button></span>
-        </div>
-        {this.state.foodsAreOpen ? <Foods foodList={meal.foods} /> : <div></div>}
+        { !this.state.update ? 
+          <div>
+            <div style={{ display: 'flex'}}>
+              <span style={{ flex: 1 }}>{this.state.title}</span>
+              <span style={{ flex: 1 }}>{this.state.day}</span>
+              <span style={{ flex: 1 }}>{this.state.date}</span>
+              <span style={{ flex: 1 }}>{this.state.time}</span>
+              <span style={{ flex: 1 }}>{this.state.totalCalories}</span>
+              <span style={{ flex: 1 }}>{this.state.health_rating}</span>
+              <span style={{ flex: 1 }}><button onClick={() => this.showFoods()}>show foods</button></span>
+              <span style={{ flex: 1 }}>
+                <button onClick={() => this.setState({ update: true })}>Edit Meal</button>
+              </span>
+              <span style={{ flex: 1 }}>
+                <button onClick={() => removeMeal(this.state.id)}>Remove Meal</button>
+              </span>
+            </div>
+            {this.state.foodsAreOpen ? <Foods foodList={this.state.foods} /> : <div></div>}
+          </div> : 
+          <div>
+            <form onSubmit={this.saveMeal}>
+              <div style={{ display: 'flex'}}>
+                <span style={{ flex: 1 }}>
+                  <input type="text" placeholder="title here" id="title" value={this.state.title} onChange={this.onUpdateText}/>
+                </span>
+                <span style={{ flex: 1 }}>
+                  <input type="text" placeholder="day here" id="day" value={this.state.day} onChange={this.onUpdateText}/>
+                </span>
+                <span style={{ flex: 1 }}>
+                  <input type="text" placeholder="date here" id="date" value={this.state.date} onChange={this.onUpdateText}/>
+                </span>
+                <span style={{ flex: 1 }}>
+                  <input type="text" placeholder="time here" id="time" value={this.state.time} onChange={this.onUpdateText}/>
+                </span>
+                <span style={{ flex: 1 }}>
+                  <input type="text" placeholder="total_calories here" id="total_calories" value={this.state.total_calories} onChange={this.onUpdateText}/>
+                </span>
+                <span style={{ flex: 1 }}>
+                  <input type="text" placeholder="health_rating here" id="health_rating" value={this.state.health_rating} onChange={this.onUpdateText}/>
+                </span>
+                <span style={{ flex: 1 }}>
+                  add foods
+                </span>
+                <span style={{ flex: 1 }}>
+                  <button type="submit">Save Meal</button>
+                </span>
+                <span style={{ flex: 1 }}>
+                  <button onClick={() => removeMeal(this.state.id)}>Remove Meal</button>
+                </span>
+              </div>
+            </form>
+          </div>
+        }
       </div>
     );
   }
